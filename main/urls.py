a365bcdf24abb89002b6e682fe9e665f8840feaa
sebastian-ml <sebastian2549@wp.pl>
2020-03-28 19:43:54 +0100

    Create task model and superuser Change function view to class based view. Create some tasks in admin panel.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.main, name='main-page'),
+    path('', views.TaskListView.as_view(), name='main-page'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Task
 
 
-def main(request):
-    return render(request, 'main/home.html')
+class TaskListView(ListView):
+    model = Task
+    template_name = 'main/home.html'
